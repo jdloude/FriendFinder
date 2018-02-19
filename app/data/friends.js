a4978bbf11d5f0ab70693a5b874dc66faeeeca67
Joshua Loudermilk <jdloude@gmail.com>
2018-02-18 21:09:56 -0700

    v1.1

--- app/data/friends.js
+++ app/data/friends.js
@@ -1,18 +1,7 @@
-var friends = [{
-    "name": "Josh",
-    "photo": "https://scontent.fapa1-1.fna.fbcdn.net/v/t1.0-9/10547622_10152857757409561_7365472398094570474_n.jpg?oh=5d74a2ccd8b67a96d501360d4ffa60bd&oe=5B4D79C1",
-    "scores": [
-        "2",
-        "1",
-        "5",
-        "1",
-        "2",
-        "4",
-        "2",
-        "2",
-        "1",
-        "2"
-    ]
+var friendsArray = [{
+    name: "Josh",
+    photo: "https://scontent.fapa1-1.fna.fbcdn.net/v/t1.0-9/10547622_10152857757409561_7365472398094570474_n.jpg?oh=5d74a2ccd8b67a96d501360d4ffa60bd&oe=5B4D79C1",
+    scores: [5, 5, 5, 5, 5, 5, 5, 5, 5, 5]
 }];
 
-module.exports = friends;
+module.exports = friendsArray;
--- app/routing/apiRoutes.js
+++ app/routing/apiRoutes.js
@@ -1,62 +1,38 @@
-// File to handle the friend matching logic
+var friendsArray = require("../data/friends");
 var path = require("path");
-var friends = require("../data/friends.js");
-
-// Display friends on /api/friends route
 
 module.exports = function(app) {
     app.get("/api/friends", function(req, res) {
-        res.json(friends);
+        res.json(friendsArray);
     });
 
-    // Post request to manipulate user's data and send back a new friend match
-
     app.post("/api/friends", function(req, res) {
+        // Code to find best match
 
-        var surveyResults = req.body;
+        // Initialize array to hold comparison results
+        var logic = 10000000;
+        var bestFriend;
 
-        surveyResults.name = surveyResults.name.replace(/\s+/g, "");
+        //For loop through each friend in friends
+        for (var i = 0; i < friendsArray.length; i++) {
+            // Array to total the differences in scores for potential matches
+            var compArray = [];
 
-        surveyResults.photo = surveyResults.photo.replace(/\s+/g, "");
-
-        // variables to store matched friend data
-        var newUserScores = +surveyResults.scores;
-        var matchName = '';
-        var matchImage = '';
-
-        // Starter number in order to make logic below work
-        var totalDifference = 10000;
-
-        // Iterate through the friends array
-        for (var i = 0; i < friends.length; i++) {
-            // Initialize difference b/t scores for a particular friend
-            var diff = 0;
-            // Iterate through each survey score of a friend
-            for (var j = 0; j < friends[i].scores.length; j++) {
-                // Difference = absolute value of potential new friend scores minus scores inputted by new user
-                diff += Math.abs(friends[i].scores[j]) - (newUserScores[j]);
+            for (var j = 0; j < friendsArray[i].scores.length; j++) {
+                compArray.push(Math.abs(friendsArray[i].scores[j] - req.body.scores[j]));
             }
-            // If this difference is less than the global score difference...
-            if (diff < totalDifference) {
-                // This is the new best friend for now...
-                totalDifference = diff;
-                // Set name and photo to this friend's property values
-                matchName = friends[i].name;
-                matchImage = friends[i].photo;
+            var matchScore = compArray.reduce((a, b) => a + b, 0);
+
+            if (matchScore < logic) {
+                logic = matchScore;
+                bestFriend = friendsArray[i];
             }
         }
 
-        // Add user's inputs into the friends array
-        friends.push(surveyResults);
+        // Return best match to client
+        res.json(bestFriend);
 
-        // Respond with json of name and photo of the matched friend
-        res.json({
-            status: 'OK',
-            matchName: matchName,
-            matchImage: matchImage
-        });
-
-
+        // Add current user to friendsArray
+        friendsArray.push(req.body);
     });
-
 };
