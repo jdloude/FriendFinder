a6409b6ac15219820bda6a2182bf2ad8a7353903
Joshua Loudermilk <jdloude@gmail.com>
2018-02-19 21:42:31 -0700

    v1.2

--- app/data/friends.js
+++ app/data/friends.js
@@ -1,7 +1,18 @@
 var friendsArray = [{
-    name: "Josh",
-    photo: "https://scontent.fapa1-1.fna.fbcdn.net/v/t1.0-9/10547622_10152857757409561_7365472398094570474_n.jpg?oh=5d74a2ccd8b67a96d501360d4ffa60bd&oe=5B4D79C1",
-    scores: [5, 5, 5, 5, 5, 5, 5, 5, 5, 5]
-}];
+        name: "Josh",
+        photo: "https://scontent.fapa1-1.fna.fbcdn.net/v/t1.0-9/10547622_10152857757409561_7365472398094570474_n.jpg?oh=5d74a2ccd8b67a96d501360d4ffa60bd&oe=5B4D79C1",
+        scores: [5, 5, 5, 5, 5, 5, 5, 5, 5, 5]
+    },
+    {
+        name: "The Dude",
+        photo: "https://www.mountaineers.org/images/placeholder-images/placeholder-400-x-400",
+        scores: [2, 5, 4, 2, 3, 3, 5, 2, 5, 4]
+    },
+    {
+        name: "Dhat Guy",
+        photo: "https://www.mountaineers.org/images/placeholder-images/placeholder-400-x-400",
+        scores: [3, 4, 5, 1, 5, 3, 2, 5, 2, 1]
+    }
+];
 
 module.exports = friendsArray;
--- app/routing/apiRoutes.js
+++ app/routing/apiRoutes.js
@@ -10,27 +10,46 @@ module.exports = function(app) {
         // Code to find best match
 
         // Initialize array to hold comparison results
-        var logic = 10000000;
+        var logic = 100;
         var bestFriend;
+        var newFriendArry = [];
+
+        var newFriendScores = Object.values(req.body)[2];
+
+        newFriendScores.forEach(function(element) {
+            parseInt(element);
+            newFriendArry.push(element);
+        });
+
+        var newFriendScore = newFriendArry.reduce((a, b) => a + b, 0);
+        //console.log(newFriendArry);
 
         //For loop through each friend in friends
         for (var i = 0; i < friendsArray.length; i++) {
+
             // Array to total the differences in scores for potential matches
             var compArray = [];
 
-            for (var j = 0; j < friendsArray[i].scores.length; j++) {
-                compArray.push(Math.abs(friendsArray[i].scores[j] - req.body.scores[j]));
-            }
+            var friendsArrayScores = Object.values(friendsArray[i])[2];
+
+            friendsArrayScores.forEach(function(element) {
+                parseInt(element);
+                compArray.push(element);
+            });
+
             var matchScore = compArray.reduce((a, b) => a + b, 0);
+
+            //console.log(matchScore);
 
             if (matchScore < logic) {
                 logic = matchScore;
-                bestFriend = friendsArray[i];
+                bestFriend = friendsArray[0];
             }
         }
 
         // Return best match to client
         res.json(bestFriend);
+
 
         // Add current user to friendsArray
         friendsArray.push(req.body);
